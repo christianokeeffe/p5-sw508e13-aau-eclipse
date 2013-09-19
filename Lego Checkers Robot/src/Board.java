81397c8b896b7c289c460421db3525dfdf28c3a8
rasmus.gadensgaard@gmail.com <rasmus.gadensgaard@gmail.com@b5a7653f-1138-106a-5eed-3aa86ebcedbe>
2013-09-19 09:33:52 +0000

    King moved handling

---
+++ Lego Checkers Robot/src/Board.java
@@ -0,0 +1,205 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import lejos.nxt.ColorSensor;
+
+
+public class Board {
+	List<List<Field>> myBoard;
+	char myColor;
+	
+	public Board() throws InterruptedException, IOException
+	{
+		myColor = this.findMyColor();
+
+			int j,i;
+			
+			for(i=0;i<8;i++){
+				List<Field> Columns = new ArrayList<Field>();
+				for(j=0;j<8;j++)
+				{
+					Field temp = new Field();
+					if((i+j)%2 == 0)
+					{
+						temp.allowedField = true;
+						
+						if(i < 3)
+						{
+							if(i == 2)
+							{
+								temp.moveable = true;
+							}
+							temp.pieceColor = myColor;
+						}
+						else if(i > 4)
+						{
+							if(i == 5)
+							{
+								temp.moveable = true;
+							}
+							if(myColor != 'r')
+							{
+								temp.pieceColor = 'w';
+							}
+							else
+							{
+								temp.pieceColor = myColor;
+							}
+						}
+					}
+					else
+					{
+						temp.allowedField = false;
+					}
+					Columns.add(temp);
+				}
+				myBoard.add(Columns);
+		}
+	}
+	
+	
+	public boolean analyzeBoard() throws InterruptedException, IOException
+	{
+		myBoard.get(3).get(3).isKing = true;
+		myBoard.get(3).get(3).moveable = true;
+		myBoard.get(3).get(3).pieceColor = 'w';
+		
+		int i=0, j=0;
+		
+		for (List<Field> f : myBoard) {
+			for (Field field : f) {
+			    if(field.moveable)
+			    {
+			    	if(this.isEmptyField(i, j))
+			    	{
+						if(myBoard.get(i).get(j).isKing)
+						{
+							if((i > 0 && i < 7) && (j > 0 && j < 7))
+							{
+								if(!this.isEmptyField(i-1, j-1))
+								{
+									//King moved here
+								}
+								else if(!this.isEmptyField(i+1, j-1))
+								{
+									//King moved here
+								}
+								else if(!this.isEmptyField(i+1, j+1))
+								{
+									//King moved here
+								}
+								else if(!this.isEmptyField(i-1, j+1))
+								{
+									//King moved here
+								}
+							}
+							else if(i==0 && j==0)
+							{
+								if(!this.isEmptyField(i+1, j+1))
+								{
+									//King moved here
+								}
+							}
+							else if(i==7 && j==7)
+							{
+								if(!this.isEmptyField(i-1, j-1))
+								{
+									//King moved here
+								}
+							}
+							else if(i == 0 && j!=0 && j!= 7)
+							{
+								if(!this.isEmptyField(i+1, j+1))
+								{
+									//King moved here
+								}
+								if(!this.isEmptyField(i+1, j-1))
+								{
+									//King moved here
+								}
+							}
+							else if(j == 0 && i!=0 && i!= 7)
+							{
+								if(!this.isEmptyField(i+1, j+1))
+								{
+									//King moved here
+								}
+								if(!this.isEmptyField(i-1, j+1))
+								{
+									//King moved here
+								}
+							}
+							else if(j == 7 && i!=0 && i!= 7)
+							{
+								if(!this.isEmptyField(i+1, j-1))
+								{
+									//King moved here
+								}
+								if(!this.isEmptyField(i-1, j-1))
+								{
+									//King moved here
+								}
+							}
+							else if(i == 7 && j!=0 && j!= 7)
+							{
+								if(!this.isEmptyField(i-1, j-1))
+								{
+									//King moved here
+								}
+								if(!this.isEmptyField(i-1, j+1))
+								{
+									//King moved here
+								}
+							}
+						}
+			    	}
+			    }
+			    j++;
+			}
+			i++;
+		}
+		return true;
+	}
+	
+	public boolean isEmptyField(int x, int y) throws InterruptedException, IOException
+	{
+		RemoteNXTFunctions remoteFunctions = new RemoteNXTFunctions();
+		
+		ColorSensor.Color colorResult = remoteFunctions.GetColorOnField(x, y);
+		
+		int red = colorResult.getRed();
+		int green = colorResult.getGreen();
+		int blue = colorResult.getBlue();
+		
+		if(red < 50 && green < 50 && blue < 50)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	
+	public char findMyColor() throws InterruptedException, IOException
+	{
+		RemoteNXTFunctions remoteFunctions = new RemoteNXTFunctions();
+		
+		ColorSensor.Color colorResult = remoteFunctions.GetColorOnField(0, 0);
+		
+		int red = colorResult.getRed();
+		int green = colorResult.getGreen();
+		int blue = colorResult.getBlue();
+		
+		if(red > 150 && green <= 50 && blue <= 50)
+		{
+			return 'r';
+		}
+		else
+		{
+			return 'w';
+		}
+	}
+}
---
+++ Lego Checkers Robot/src/Field.java
@@ -0,0 +1,7 @@
+
+public class Field {
+	public char pieceColor;
+	public boolean allowedField;
+	public boolean moveable = false;
+	public boolean isKing = false;
+}
