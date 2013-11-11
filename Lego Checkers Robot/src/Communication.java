77d6bbbbd2d1ded8aeafa0ab6165078a8ca673fe
rasmus.gadensgaard@gmail.com <rasmus.gadensgaard@gmail.com@b5a7653f-1138-106a-5eed-3aa86ebcedbe>
2013-11-11 14:09:22 +0000

    added Communication

---
+++ Lego Checkers Robot/src/Communication.java
@@ -0,0 +1,52 @@
+import lejos.nxt.LCD;
+import lejos.nxt.Sound;
+
+public class Communication {
+    public final void playYourTurn() {
+        Sound.twoBeeps();
+        /*File soundFile = new File("yourturn.wav");
+        Sound.playSample(soundFile, 100); // 100 ... volume*/
+    }
+
+    public final void illeagalMove() {
+        Sound.buzz();
+        LCD.clear();
+        LCD.drawString("Illegal move", 0, 0);
+        LCD.refresh();
+    }
+
+    public final void humanWon() {
+        Sound.beepSequenceUp();;
+        LCD.clear();
+        LCD.drawString("You won!", 0, 0);
+        LCD.refresh();
+    }
+
+    public final void robotWon() {
+        Sound.beepSequence();;
+        LCD.clear();
+        LCD.drawString("You lost!", 0, 0);
+        LCD.refresh();
+    }
+
+    public final void draw() {
+        Sound.beep();
+        LCD.clear();
+        LCD.drawString("You got draw!", 0, 0);
+        LCD.refresh();
+    }
+
+    public final void myKingNotPlaced() {
+        Sound.buzz();
+        LCD.clear();
+        LCD.drawString("Place my king", 0, 0);
+        LCD.refresh();
+    }
+
+    public final void nothingPossible() {
+        Sound.buzz();
+        LCD.clear();
+        LCD.drawString("No possible movement", 0, 0);
+        LCD.refresh();
+    }
+}
