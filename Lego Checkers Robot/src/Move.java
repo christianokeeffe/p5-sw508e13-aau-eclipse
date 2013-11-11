913b91815185ab750ac630cf73b54041e6d7a9ae
christianokeeffe <christianokeeffe@b5a7653f-1138-106a-5eed-3aa86ebcedbe>
2013-11-11 11:55:51 +0000

    Stylecheck af move og piece

--- Lego Checkers Robot/src/Move.java
+++ Lego Checkers Robot/src/Move.java
@@ -5,49 +5,40 @@ public class Move {
     //public boolean isJump;
     public Stack<Piece> takenPieces = new Stack<Piece>();
     public boolean wasKingBefore = false;
-    
-    Move(Field movefrom, Field moveto, boolean wasKing)
-    {
+
+    Move(Field movefrom, Field moveto, boolean wasKing) {
         Stack<Field> moveToList = new Stack<Field>();
-        
         moveToList.push(moveto);
         moveToList.push(movefrom);
         this.moves = moveToList;
         this.wasKingBefore = wasKing;
     }
-    
-    Move(Stack<Field> moveToList, boolean wasKing)
-    {
+
+    Move(Stack<Field> moveToList, boolean wasKing) {
         this.moves = moveToList;
         this.wasKingBefore = wasKing;
     }
-    
-    public boolean isJump()
-    {
-        if(moves.size() >= 2)
-        {
-            Field from = moves.pop();
-            Field to = moves.peek();
-            moves.push(from);
-            
-            if(Math.abs(from.x - to.x) == 2 && Math.abs(from.y - to.y) == 2)
-                return true;
-            return false;
-        }
-        else
-        {
-            return false;
-        }
-    }
-    
-    Move()
-    {
+
+    Move() {
         Stack<Field> moveToList = new Stack<Field>();
         this.moves = moveToList;
     }
-    
-    public void addStep(Field step)
-    {
+
+    public final boolean isJump() {
+        if (moves.size() >= 2) {
+            Field from = moves.pop();
+            Field to = moves.peek();
+            moves.push(from);
+            if (Math.abs(from.x - to.x) == 2 && Math.abs(from.y - to.y) == 2) {
+                return true;
+            }
+            return false;
+        } else {
+            return false;
+        }
+    }
+
+    public final void addStep(Field step) {
         this.moves.push(step);
     }
 }
