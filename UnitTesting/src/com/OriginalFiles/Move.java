8cb718cdb289131a6ab6a2a83f972fea6f4649e2
rasmus.gadensgaard@gmail.com <rasmus.gadensgaard@gmail.com@b5a7653f-1138-106a-5eed-3aa86ebcedbe>
2013-11-14 09:42:44 +0000

    Added unit test for Move revision 408

---
+++ UnitTesting/src/com/OriginalFiles/Move.java
@@ -0,0 +1,44 @@
+package com.OriginalFiles;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Move {
+    public List<Field> moves;
+    //public boolean isJump;
+    public List<Piece> takenPieces = new ArrayList<Piece>();
+    public boolean wasKingBefore = false;
+
+    public Move(Field movefrom, Field moveto, boolean wasKing) {
+        List<Field> moveToList = new ArrayList<Field>();
+        moveToList.add(movefrom);
+        moveToList.add(moveto);
+        this.moves = moveToList;
+        this.wasKingBefore = wasKing;
+    }
+
+    public Move(List<Field> moveToList, boolean wasKing) {
+        this.moves = moveToList;
+        this.wasKingBefore = wasKing;
+    }
+
+   public Move() {
+        List<Field> moveToList = new ArrayList<Field>();
+        this.moves = moveToList;
+    }
+
+    public final boolean isJump() {
+        if (moves.size() >= 2) {
+            Field to = moves.get(1);
+            Field from  = moves.get(0);
+
+            return Math.abs(from.x - to.x) == 2 && Math.abs(from.y - to.y) == 2;
+        } else {
+            return false;
+        }
+    }
+
+    public final void addStep(Field step) {
+        this.moves.add(step);
+    }
+}
+
