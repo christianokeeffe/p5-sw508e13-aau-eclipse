5915c8b03764faca01bed0f55a8a5d2e6c49db3f
niels@jrkm.dk <niels@jrkm.dk@b5a7653f-1138-106a-5eed-3aa86ebcedbe>
2013-11-18 11:52:07 +0000

    laver retterser

--- UnitTesting/src/com/Testing/AnalyzeTest.java
+++ UnitTesting/src/com/Testing/AnalyzeTest.java
@@ -8,28 +8,27 @@ import custom.Exceptions.IllegalMove;
 import custom.Exceptions.NoKingLeft;
 
 public class AnalyzeTest extends FunktionForTesting {
-    com.OriginalFiles.Analyze analyzeForTest;
+    
     public AnalyzeTest() throws InterruptedException, IOException {
         super();
-        analyzeForTest = checkersBoard.analyzeFunctions;
         // TODO Auto-generated constructor stub
     }
 
     @Test
-    public final void testGameHasEnded() throws InterruptedException, IOException {
+    public final void testGameHasEnded() throws InterruptedException, IOException, NoKingLeft, IllegalMove {
         //test if the game is not ended with start positioning
-        assertEquals(0, analyzeForTest.gameHasEnded(true));
-        assertEquals(0, analyzeForTest.gameHasEnded(false));
+        assertEquals(0, checkersBoard.analyzeFunctions.gameHasEnded(true));
+        assertEquals(0, checkersBoard.analyzeFunctions.gameHasEnded(false));
 
         constructWinCase(true);
         //test if the game registers that the human have won independent of whos turn it is
-        assertEquals(1, analyzeForTest.gameHasEnded(true));
-        assertEquals(1, analyzeForTest.gameHasEnded(false));
+        assertEquals(1, checkersBoard.analyzeFunctions.gameHasEnded(true));
+        assertEquals(1, checkersBoard.analyzeFunctions.gameHasEnded(false));
         
         constructWinCase(false);
         //test if the game registers that the robot have won independent of whos turn it is
-        assertEquals(2, analyzeForTest.gameHasEnded(true));
-        assertEquals(2, analyzeForTest.gameHasEnded(false));
+        assertEquals(2, checkersBoard.analyzeFunctions.gameHasEnded(true));
+        assertEquals(2, checkersBoard.analyzeFunctions.gameHasEnded(false));
         
         emptyBoard();
         
@@ -38,8 +37,8 @@ public class AnalyzeTest extends FunktionForTesting {
         checkersBoard.myBoard[4][5].setPieceOnField(producePiece(4, 5, 'b', true));
         checkersBoard.myBoard[6][7].setPieceOnField(producePiece(4, 5, 'g', true));
         
-        assertEquals(3, analyzeForTest.gameHasEnded(true));
-        assertNotEquals(3, analyzeForTest.gameHasEnded(false));
+        assertEquals(3, checkersBoard.analyzeFunctions.gameHasEnded(true));
+        assertNotEquals(3, checkersBoard.analyzeFunctions.gameHasEnded(false));
         
         emptyBoard();
         
@@ -48,8 +47,8 @@ public class AnalyzeTest extends FunktionForTesting {
         checkersBoard.myBoard[1][0].setPieceOnField(producePiece(4, 5, 'b',true));
         checkersBoard.myBoard[1][2].setPieceOnField(producePiece(4, 5, 'g',true));
         
-        assertEquals(3, analyzeForTest.gameHasEnded(false));
-        assertNotEquals(3, analyzeForTest.gameHasEnded(true));
+        assertEquals(3, checkersBoard.analyzeFunctions.gameHasEnded(false));
+        assertNotEquals(3, checkersBoard.analyzeFunctions.gameHasEnded(true));
         
         resetBoard();
     }
@@ -62,18 +61,18 @@ public class AnalyzeTest extends FunktionForTesting {
     public final void testCheckForGameHasEnded() throws InterruptedException, IOException {
         
         //test if the games ends in the different scenarios
-        assertFalse(analyzeForTest.checkForGameHasEnded(true));
-        assertFalse(analyzeForTest.checkForGameHasEnded(false));
+        assertFalse(checkersBoard.analyzeFunctions.checkForGameHasEnded(true));
+        assertFalse(checkersBoard.analyzeFunctions.checkForGameHasEnded(false));
         
         constructWinCase(true);
         
-        assertTrue(analyzeForTest.checkForGameHasEnded(true));
-        assertTrue(analyzeForTest.checkForGameHasEnded(false));
+        assertTrue(checkersBoard.analyzeFunctions.checkForGameHasEnded(true));
+        assertTrue(checkersBoard.analyzeFunctions.checkForGameHasEnded(false));
         
         constructWinCase(false);
         
-        assertTrue(analyzeForTest.checkForGameHasEnded(true));
-        assertTrue(analyzeForTest.checkForGameHasEnded(false));
+        assertTrue(checkersBoard.analyzeFunctions.checkForGameHasEnded(true));
+        assertTrue(checkersBoard.analyzeFunctions.checkForGameHasEnded(false));
         
         emptyBoard();
         
@@ -81,8 +80,8 @@ public class AnalyzeTest extends FunktionForTesting {
         checkersBoard.myBoard[4][5].setPieceOnField(producePiece(4, 5, 'b',true));
         checkersBoard.myBoard[6][7].setPieceOnField(producePiece(6, 7, 'g',true));
         
-        assertTrue(analyzeForTest.checkForGameHasEnded(true));
-        assertFalse(analyzeForTest.checkForGameHasEnded(false));
+        assertTrue(checkersBoard.analyzeFunctions.checkForGameHasEnded(true));
+        assertFalse(checkersBoard.analyzeFunctions.checkForGameHasEnded(false));
         
         emptyBoard();
         
@@ -90,8 +89,8 @@ public class AnalyzeTest extends FunktionForTesting {
         checkersBoard.myBoard[1][0].setPieceOnField(producePiece(1, 0, 'b',true));
         checkersBoard.myBoard[1][2].setPieceOnField(producePiece(1, 2, 'g',true));
         
-        assertTrue(analyzeForTest.checkForGameHasEnded(false));
-        assertFalse(analyzeForTest.checkForGameHasEnded(true));
+        assertTrue(checkersBoard.analyzeFunctions.checkForGameHasEnded(false));
+        assertFalse(checkersBoard.analyzeFunctions.checkForGameHasEnded(true));
         
         resetBoard();
     }
@@ -101,7 +100,7 @@ public class AnalyzeTest extends FunktionForTesting {
         boolean testException = false;
         
         //test if analyzeboard works when moving a piece
-        assertTrue(analyzeForTest.analyzeBoard());
+        assertTrue(checkersBoard.analyzeFunctions.analyzeBoard());
         
         //production of an illegal move
         checkersBoard.myBoard[3][4].setPieceOnField(producePiece(4, 5, 'r',false));
@@ -109,7 +108,7 @@ public class AnalyzeTest extends FunktionForTesting {
         
         //test that analyzeBoard finds and reports the illegal move
         try {
-            analyzeForTest.analyzeBoard();
+            checkersBoard.analyzeFunctions.analyzeBoard();
         }
         catch (IllegalMove e) {
             testException = true;
@@ -118,14 +117,23 @@ public class AnalyzeTest extends FunktionForTesting {
         assertTrue(testException);
         
         resetBoard();
+        
+        constructWinCase(true);
+        // test if the game registers that it do not need to analyze the board if the game has ended if it humans turn
+        assertEquals(false, checkersBoard.analyzeFunctions.analyzeBoard());
+        
+        constructWinCase(false);
+        // test if the game registers that it do not need to analyze the board if the game has ended if it robots turn
+        assertEquals(false, checkersBoard.analyzeFunctions.analyzeBoard());
+        resetBoard();
     }
     
     
     //Test not that valid for the real GetColor function
     @Test
     public final void testGetColor() throws IOException {
-        assertEquals(checkersBoard.myBoard[1][0].getPieceOnField().color, analyzeForTest.getColor(1, 0));
-        assertEquals(' ', analyzeForTest.getColor(0, 3));
-        assertEquals('w', analyzeForTest.getColor(1,4));
+        assertEquals(checkersBoard.myBoard[1][0].getPieceOnField().color, checkersBoard.analyzeFunctions.getColor(1, 0));
+        assertEquals(' ', checkersBoard.analyzeFunctions.getColor(0, 3));
+        assertEquals('w', checkersBoard.analyzeFunctions.getColor(1,4));
     }
 }
