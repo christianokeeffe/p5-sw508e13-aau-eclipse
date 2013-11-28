d70dd39470052511f8feee0b6d0feb78a0ba75c9
aleksandersn@gmail.com <aleksandersn@gmail.com@b5a7653f-1138-106a-5eed-3aa86ebcedbe>
2013-11-28 08:45:33 +0000

    light

--- Lego Checkers Robot/src/Communication.java
+++ Lego Checkers Robot/src/Communication.java
@@ -9,11 +9,12 @@ public class Communication {
 
     Communication(RemoteNXTFunctions inputNxt) {
         nxt = inputNxt;
+        nxt.bottomNXT.C.setSpeed(900);
     }
 
     public final void playYourTurn() {
         Sound.twoBeeps();
-        nxt.bottomNXT.C.setPower(-100);
+        nxt.bottomNXT.C.forward();
         LCD.clear();
         LCD.drawString("Your turn", 0, 0);
         LCD.refresh();
@@ -75,7 +76,7 @@ public class Communication {
     }
 
     public final void robotTurn() {
-        nxt.bottomNXT.C.setPower(0);
+        nxt.bottomNXT.C.flt(true);
         LCD.clear();
         LCD.drawString("Please wait", 0, 0);
         LCD.refresh();
